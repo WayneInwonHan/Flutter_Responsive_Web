7d375fd54b2d6adaeb615ad1f422dafc49357d31
WayneInwonHan <blissgram@gmail.com>
2021-04-26 23:35:36 -0500

    Fix case where landing section overflowed, and Update responsive structures

--- lib/sections/S0_headerSection/S0_header_section.dart
+++ lib/sections/S0_headerSection/S0_header_section.dart
@@ -4,6 +4,22 @@ import 'package:inonepage_landing/widgets/animation_logo.dart';
 class HeaderSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constratinsts) {
+      if (constratinsts.maxWidth > 1200) {
+        return DesktopHeaderSection();
+      } else if (constratinsts.maxWidth > 800 &&
+          constratinsts.maxWidth < 1200) {
+        return DesktopHeaderSection();
+      } else {
+        return MobileHeaderSection();
+      }
+    });
+  }
+}
+
+class DesktopHeaderSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 80),
       child: Container(
@@ -35,6 +51,57 @@ class HeaderSection extends StatelessWidget {
                     onPressed: () {},
                     child: Image.asset('assets/images/linkedin_rect.png')),
               ]),
+            ]),
+      ),
+    );
+  }
+}
+
+class MobileHeaderSection extends StatelessWidget {
+  const MobileHeaderSection({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      child: Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              AnimationLogo(),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(children: <Widget>[
+                      TextButton(
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                            padding: EdgeInsets.all(0.0),
+                          ),
+                          onPressed: () {},
+                          child: Image.asset('assets/images/youtube_play.png')),
+                      TextButton(
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                            padding: EdgeInsets.all(0.0),
+                          ),
+                          onPressed: () {},
+                          child:
+                              Image.asset('assets/images/github_square.png')),
+                      TextButton(
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                            padding: EdgeInsets.all(0.0),
+                          ),
+                          onPressed: () {},
+                          child:
+                              Image.asset('assets/images/linkedin_rect.png')),
+                    ]),
+                  ],
+                ),
+              ),
             ]),
       ),
     );
