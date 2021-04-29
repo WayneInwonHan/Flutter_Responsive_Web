e799417c51bf340e5ed459312c623db06aa3ea96
WayneInwonHan <blissgram@gmail.com>
2021-04-28 23:49:42 -0500

    Add NotFoundPage, and link it to 'onUnknownRoute'

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:inonepage_landing/constants.dart';
 import 'package:inonepage_landing/home_screen.dart';
+import 'package:inonepage_landing/page404.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +17,9 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => MyHomePage(),
       },
+      onUnknownRoute: (setting) => MaterialPageRoute(
+        builder: (context) => NotFoundPage(),
+      ),
       theme: ThemeData(
         inputDecorationTheme: kDefaultInputDecorationTheme,
         visualDensity: VisualDensity.adaptivePlatformDensity,
